1556a8e9e14eacd3ea09446b4fa5a9829c4e3171
voyager003 <miraclex000@gmail.com>
2026-02-23 15:20:02 +0900

    feat: DatabaseModule 추가 (TypeORM + Mongoose 비동기 연결)
    - TypeORM forRootAsync: PostgreSQL 연결, connection pooling (max 20)
    - Mongoose forRootAsync: MongoDB 연결
    - ConfigService 주입으로 환경변수 기반 설정

---
+++ src/database/database.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.database'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('app.nodeEnv') !== 'production',
+        extra: {
+          max: 20,
+          idleTimeoutMillis: 30000,
+          connectionTimeoutMillis: 5000,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('mongodb.uri'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
